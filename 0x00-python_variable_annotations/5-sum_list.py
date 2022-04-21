4d11e7e58346e565b4037d835bbeaa9ff19d049e
FrancoBuenoHolberton <3290@holbertonschool.com>
2022-04-21 22:46:16 +0000

    task-5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ lista """
+from typing import List
 
-
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ list to sum float"""
     return sum(input_list)
