7dd5a079cdeee24d8f63d99a412293599de2feb2
FrancoBuenoHolberton <3290@holbertonschool.com>
2022-04-22 00:01:38 +0000

    task-101

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""yping Python"""
+
+from typing import Any, Sequence, Union, TypeVar, Mapping
+
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any, default:
+                     Union[T, None]) -> Union[Any, T]:
+    """-----"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
