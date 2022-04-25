181ca91f488044348c6c7e9cf21f808f2f99b1c0
root <3290@holbertonschool.com>
2022-04-25 19:50:49 +0000

    task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Comentario pa agregar"""
+
+import asyncio
+import random
+
+async def wait_random(max_delay: int = 10) -> float:
+    """take in the argument , the delay and return the delay"""
+    randon = random.uniform(0, max_delay)
+    await asyncio.sleep(randon)
+    return randon
+    
---
+++ 0x01-python_async_function/README.md
@@ -0,0 +1,1 @@
+Hi functions ASYNC
