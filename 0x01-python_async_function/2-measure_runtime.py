4434882f7c2572c90abd6d03f0cf8579e71c8138
root <3290@holbertonschool.com>
2022-04-25 20:47:15 +0000

    task-2

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""  Func """
+
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """returns time total"""
+    inicio = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    tiempo-total = time.perf_counter() - inicio
+    return tiempo-total / n
