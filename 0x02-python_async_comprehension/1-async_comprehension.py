a73adf5530bfcdf27c8046a5f8949108dca8e479
root <3290@holbertonschool.com>
2022-04-27 00:02:34 +0000

    task-fix1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -6,7 +6,7 @@ async_generator = __import__('0-async_generator').async_generator
 Vector = List[float]
 
 
-async def async_comprehension -> Vector:
+async def async_comprehension() -> Vector:
     """ return async func """
     tareal = [i async for i in async_generator()]
     return tareal
